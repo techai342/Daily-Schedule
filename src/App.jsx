05dad57a94bcaed6c969c4d82f3308bfdc82da00
techai342 <tiktokusr4400@gmail.com>
2025-11-02 14:06:31 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,22 @@ import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
 import AddTask from "./pages/AddTask";
 import Reports from "./pages/Reports";
+import ScheduleTracker from "./pages/ScheduleTracker";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/add-task" element={<AddTask />} />
-        <Route path="/reports" element={<Reports />} />
-      </Routes>
-      <Footer />
+      <div className="min-h-screen bg-ios-bg">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/add-task" element={<AddTask />} />
+          <Route path="/reports" element={<Reports />} />
+          <Route path="/schedule" element={<ScheduleTracker />} />
+        </Routes>
+        <Footer />
+      </div>
     </BrowserRouter>
   );
 }
-
